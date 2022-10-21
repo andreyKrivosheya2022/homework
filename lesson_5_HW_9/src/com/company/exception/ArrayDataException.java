7e54d0845986206439729bd63496c0dd05e98883
Andrey Krivosheya <andreykrivosheya786@gmail.com>
2022-10-21 23:17:42 +0300

    Second commit

--- lesson_5_HW_9/src/com/company/exception/ArrayDataException.java
+++ lesson_5_HW_9/src/com/company/exception/ArrayDataException.java
@@ -5,5 +5,4 @@ public class ArrayDataException extends Exception {
     public ArrayDataException(String message) {
         super(message);
     }
-
 }
--- lesson_5_HW_9/src/com/company/exception/ArraySizeException.java
+++ lesson_5_HW_9/src/com/company/exception/ArraySizeException.java
@@ -1,6 +1,7 @@
 package com.company.exception;
 
 public class ArraySizeException extends Exception {
+
     public ArraySizeException(String message) {
      super(message);
     }
