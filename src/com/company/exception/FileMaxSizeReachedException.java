a84ce6eae16d7f46cd12651c8c03c3f7a1de8e44
andreyKrivosheya2022 <115372796+andreyKrivosheya2022@users.noreply.github.com>
2022-10-31 14:30:21 +0200

    Revert "Add files via upload"

--- src/com/company/exception/FileMaxSizeReachedException.java
+++
@@ -1,7 +0,0 @@
-package com.company.exception;
-
-public class FileMaxSizeReachedException extends Exception {
-    public FileMaxSizeReachedException(String message) {
-        super(message);
-    }
-}
