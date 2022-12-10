0d932155bba027a00a78d5d4e6f641eb6e2cbbe9
andreyKrivosheya2022 <115372796+andreyKrivosheya2022@users.noreply.github.com>
2022-12-10 20:08:44 +0200

    Add files via upload

---
+++ HW_15/src/coffee/order/Order.java
@@ -0,0 +1,25 @@
+package coffee.order;
+
+public class Order {
+    private final String name;
+    private final Integer orderNumber;
+
+    public Order(String name, Integer orderNumber) {
+        this.name = name;
+        this.orderNumber = orderNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getNumber() {
+        return orderNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "orderNumber = " + orderNumber +
+                ", Name = '" + name + '\'';
+    }
+}
