ae446bb34f81bdb7b8ce773fac3cc2f81a6fdbfe
andreyKrivosheya2022 <115372796+andreyKrivosheya2022@users.noreply.github.com>
2023-05-03 12:40:30 +0300

    Add files via upload

---
+++ spring_web_servlet/src/main/java/com/example/spring_web_servlet/model/Order.java
@@ -0,0 +1,23 @@
+package com.example.spring_web_servlet.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class Order {
+    private Long id;
+    private LocalDateTime date;
+    private BigDecimal cost;
+    private List<Product> products;
+
+    public Order(Long id, LocalDateTime date, BigDecimal cost, List<Product> products) {
+        this.id = id;
+        this.date = date;
+        this.cost = cost;
+        this.products = products;
+    }
+}
